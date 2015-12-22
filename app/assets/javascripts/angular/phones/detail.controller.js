b95524a8dcf1d60010b9472ac509134275c92f2d
Ann <anna.annanx@yandex.ru>
2015-12-22 15:21:04 +0300

    Добавила промисы в detail

--- app/assets/javascripts/angular/phones/detail.controller.js
+++ app/assets/javascripts/angular/phones/detail.controller.js
@@ -7,10 +7,11 @@
 
   PhoneDetailCtrl.$inject = ['$scope', 'Phones', '$routeParams'];
 
-  function PhoneDetailCtrl($scope, Phones, $routeParams) {     
-
-    Phones.setPhone();
-    $scope.phones = Phones.getPhone();
+  function PhoneDetailCtrl($scope, Phones, $routeParams) {
+       
+  	Phones.setPhone().then(function(response) {
+      $scope.phones = Phones.getPhone();
+    }, null);
   }
 
 })(window.angular);
